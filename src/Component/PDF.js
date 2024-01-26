05067517728b600640dc283f3d5d8aa32b52eb6e
vrajesh0167 <chovatiyavrajesh167@gmail.com>
2024-01-26 12:07:35 +0530

    add download pdf methods

--- src/Component/PDF.js
+++ src/Component/PDF.js
@@ -1,27 +1,34 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import { usePDF } from 'react-to-pdf';
+import React from "react";
+import { useSelector } from "react-redux";
+import { usePDF } from "react-to-pdf";
 
 export default function PDF() {
     const CurrentDate = new Date(Date.now());
-    const CurrentTime = new Date().getHours() + ':' + new Date().getMinutes();
-    const { CartItem, totalQuantity, totalAmount, cgst, sgst } = useSelector(state => state.cart);
+    const CurrentTime = new Date().getHours() + ":" + new Date().getMinutes();
+    const { CartItem, totalQuantity, totalAmount, cgst, sgst } = useSelector(
+        (state) => state.cart
+    );
     const itemLenght = CartItem ? CartItem.length : 0;
     const HandlingCharges = totalAmount === 0 ? 0 : 35.24;
     const TotalPayable = totalAmount + Number(HandlingCharges) + cgst + sgst;
-    const TotalTax = Number(HandlingCharges) + cgst + sgst
-    const { toPDF, targetRef } = usePDF({ filename: 'post.pdf' })
+    const TotalTax = Number(HandlingCharges) + cgst + sgst;
+    const { toPDF, targetRef } = usePDF({ filename: "post.pdf" });
 
     return (
         <div>
-            <div ref={targetRef} className='pdf_section'>
-                <h3 className=' text-center mt-5'>Food Master</h3>
-                <h5 className=' text-center'>GST:- <span>24AASFJ0418NI21</span></h5>
-                <p className=' text-center'>DT:- <span>{CurrentDate.toLocaleDateString()}</span> TM:- <span>{CurrentTime}</span></p>
+            <div ref={targetRef} className="pdf_section">
+                <h3 className=" text-center mt-5">Food Master</h3>
+                <h5 className=" text-center">
+                    GST:- <span>24AASFJ0418NI21</span>
+                </h5>
+                <p className=" text-center">
+                    DT:- <span>{CurrentDate.toLocaleDateString()}</span> TM:-{" "}
+                    <span>{CurrentTime}</span>
+                </p>
 
-                <table className=' table '>
+                <table className=" table ">
                     <thead>
-                        <tr className=' text-center align-middle'>
+                        <tr className=" text-center align-middle">
                             <th>Item</th>
                             <th>Quantity</th>
                             <th>Price</th>
@@ -29,21 +36,28 @@ export default function PDF() {
                         </tr>
                     </thead>
                     <tbody>
-                        {
-                            CartItem && CartItem.map(item => <Tr key={item.id} item={item}></Tr>)
-                        }
+                        {CartItem &&
+                            CartItem.map((item) => <Tr key={item.id} item={item}></Tr>)}
                     </tbody>
                 </table>
 
-                <div className='NItem_NQty d-flex justify-content-around'>
-                    <p>NItem:- <span>{itemLenght}</span></p>
-                    <p>NQty:- <span>{totalQuantity}</span></p>
+                <div className="NItem_NQty d-flex justify-content-around">
+                    <p>
+                        NItem:- <span>{itemLenght}</span>
+                    </p>
+                    <p>
+                        NQty:- <span>{totalQuantity}</span>
+                    </p>
                 </div>
-                <div className=' gst_total'>
-                    <p className=' d-flex justify-content-between mb-1'>SubTotal <span>{TotalPayable.toFixed(2)}</span></p>
-                    <p className=' d-flex justify-content-between mb-1'>Taxable SubTotal <span>{totalAmount}</span></p>
+                <div className=" gst_total">
+                    <p className=" d-flex justify-content-between mb-1">
+                        SubTotal <span>{TotalPayable.toFixed(2)}</span>
+                    </p>
+                    <p className=" d-flex justify-content-between mb-1">
+                        Taxable SubTotal <span>{totalAmount}</span>
+                    </p>
 
-                    <table className=' table mb-1'>
+                    <table className=" table mb-1">
                         <thead>
                             <tr>
                                 <th>Taxable Amt</th>
@@ -55,23 +69,27 @@ export default function PDF() {
                         </thead>
                         <tbody>
                             <tr>
-                                <td className=' text-center align-middle'>{totalAmount}</td>
-                                <td className=' text-center align-middle'>{cgst}</td>
-                                <td className=' text-center align-middle'>{sgst}</td>
-                                <td className=' text-center align-middle'>{HandlingCharges}</td>
-                                <td className=' text-center align-middle'>{TotalTax}</td>
+                                <td className=" text-center align-middle">{totalAmount}</td>
+                                <td className=" text-center align-middle">{cgst}</td>
+                                <td className=" text-center align-middle">{sgst}</td>
+                                <td className=" text-center align-middle">{HandlingCharges}</td>
+                                <td className=" text-center align-middle">{TotalTax}</td>
                             </tr>
                         </tbody>
                     </table>
 
-                    <h3 className=' text-center my-3'>Final Amount: <span>₹ {TotalPayable.toFixed(2)}</span></h3>
+                    <h3 className=" text-center my-3">
+                        Final Amount: <span>₹ {TotalPayable.toFixed(2)}</span>
+                    </h3>
                 </div>
             </div>
-            <div className=' text-center'>
-                <button onClick={() => toPDF()} className='pdf-btn mb-5'>Download PDF</button>
+            <div className=" text-center">
+                <button onClick={() => toPDF()} className="pdf-btn mb-5">
+                    Download PDF
+                </button>
             </div>
         </div>
-    )
+    );
 }
 
 const Tr = (props) => {
@@ -79,10 +97,10 @@ const Tr = (props) => {
 
     return (
         <tr>
-            <td className=' text-start align-middle'>{title}</td>
-            <td className=' text-center align-middle'>{quantity}</td>
-            <td className=' text-center align-middle'>{price}</td>
-            <td className=' text-center align-middle'>{totalPrice}</td>
+            <td className=" text-start align-middle">{title}</td>
+            <td className=" text-center align-middle">{quantity}</td>
+            <td className=" text-center align-middle">{price}</td>
+            <td className=" text-center align-middle">{totalPrice}</td>
         </tr>
-    )
-}
+    );
+};
--- src/Routes/Routers.js
+++ src/Routes/Routers.js
@@ -1,33 +1,34 @@
-import React from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 
-import Home from '../Pages/Home'
-import Allfood from '../Pages/Allfood'
-import Fooddetails from '../Pages/Fooddetails'
-import Cart from '../Pages/Cart'
-import Checkout from '../Pages/Checkout'
-import Login from '../Pages/Login'
-import Register from '../Pages/Register'
-import Contact from '../Pages/Contact'
-import PDF from '../Component/PDF'
+import Home from "../Pages/Home";
+import Allfood from "../Pages/Allfood";
+import Fooddetails from "../Pages/Fooddetails";
+import Cart from "../Pages/Cart";
+import Checkout from "../Pages/Checkout";
+import Login from "../Pages/Login";
+import Register from "../Pages/Register";
+import Contact from "../Pages/Contact";
+import PDF from "../Component/PDF";
 
 export default function Routers() {
-    return (    
+    return (
         <div>
-            
-                <Routes>
-                    <Route path='/' element={<Navigate to='/home'></Navigate>}></Route>
-                    <Route path='/home' element={<Home></Home>}></Route>
-                    <Route path='/foods' element={<Allfood></Allfood>}></Route>
-                    <Route path='/foodsDetails/:id' element={<Fooddetails></Fooddetails>}></Route>
-                    <Route path='/cart' element={<Cart></Cart>}></Route>
-                    <Route path='/checkout' element={<Checkout></Checkout>}></Route>
-                    <Route path='/login' element={<Login></Login>}></Route>
-                    <Route path='/register' element={<Register></Register>}></Route>
-                    <Route path='/contact' element={<Contact></Contact>}></Route>
-                    <Route path='/pdf' element={<PDF></PDF>}></Route>
-                </Routes>
-            
+            <Routes>
+                <Route path="/" element={<Navigate to="/home"></Navigate>}></Route>
+                <Route path="/home" element={<Home></Home>}></Route>
+                <Route path="/foods" element={<Allfood></Allfood>}></Route>
+                <Route
+                    path="/foodsDetails/:id"
+                    element={<Fooddetails></Fooddetails>}
+                ></Route>
+                <Route path="/cart" element={<Cart></Cart>}></Route>
+                <Route path="/checkout" element={<Checkout></Checkout>}></Route>
+                <Route path="/login" element={<Login></Login>}></Route>
+                <Route path="/register" element={<Register></Register>}></Route>
+                <Route path="/contact" element={<Contact></Contact>}></Route>
+                <Route path="/pdf" element={<PDF></PDF>}></Route>
+            </Routes>
         </div>
-    )
+    );
 }
